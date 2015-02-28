9d3eb019d1752422f42681361b6e5ff90400b391
dimitriou <dimitriou@ict.tuwien.ac.at>
2015-02-27 22:47:18 -0800

    First commit

---
+++ sameDayDelivery/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  sameDayDelivery
+//
+//  Created by Pavlos Dimitriou on 2/27/15.
+//  Copyright (c) 2015 Pavlos Dimitriou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ sameDayDelivery/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  sameDayDelivery
+//
+//  Created by Pavlos Dimitriou on 2/27/15.
+//  Copyright (c) 2015 Pavlos Dimitriou. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
